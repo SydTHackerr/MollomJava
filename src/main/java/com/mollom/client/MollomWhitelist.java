44d4b03d0ba64c812f5b91bf6abc795261e34694
Thomas Meire <thomas@mollom.com>
2013-02-10 15:15:36 +0100

    Add whitelist classes

---
+++ src/main/java/com/mollom/client/MollomWhitelist.java
@@ -0,0 +1,62 @@
+/**
+ * Copyright (c) 2010-2012 Mollom. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *   * Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright notice,
+ *     this list of conditions and the following disclaimer in the documentation
+ *     and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.mollom.client;
+
+import com.mollom.client.rest.WhitelistEntry;
+import java.util.List;
+
+/**
+ *
+ */
+public class MollomWhitelist extends Mollom {
+
+  public MollomWhitelist (String publicKey, String privateKey) {
+    super(publicKey, privateKey, false);
+  }
+
+  public MollomWhitelist (String publicKey, String privateKey, boolean testing) {
+    super(publicKey, privateKey, testing);
+  }
+
+  public WhitelistEntry add (WhitelistEntry entry) {
+    throw new UnsupportedOperationException();
+  }
+
+  public WhitelistEntry get (String id) {
+    throw new UnsupportedOperationException();
+  }
+
+  public WhitelistEntry update (WhitelistEntry entry) {
+    throw new UnsupportedOperationException();
+  }
+
+  public WhitelistEntry delete (WhitelistEntry entry) {
+    throw new UnsupportedOperationException();
+  }
+
+  public List<WhitelistEntry> list () {
+    throw new UnsupportedOperationException();
+  }
+}
